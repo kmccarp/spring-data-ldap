744a09041d54fd2195a768617c910077ec9faa31
Kevin McCarpenter™️ <kevin@moderne.io>
2023-02-28 17:44:13 +0000

    refactor: Common static analysis issues
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/springframework/data/ldap/repository/support/QuerydslLdapPredicateExecutorUnitTests.java
+++ src/test/java/org/springframework/data/ldap/repository/support/QuerydslLdapPredicateExecutorUnitTests.java
@@ -56,7 +56,8 @@ class QuerydslLdapPredicateExecutorUnitTests {
 
 	@Mock LdapOperations ldapOperations;
 
-	UnitTestPerson walter, hank;
+	UnitTestPerson walter;
+	UnitTestPerson hank;
 
 	QuerydslLdapPredicateExecutor<UnitTestPerson> repository;
 
